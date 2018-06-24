10f2170431b715a35b0380ab63b2de5807a9c33a
Luc Hariman Randrianomenjanahary <luchariman@gmail.com>
2018-06-25 01:42:57 +0200

    Showing animated 3D tag cloud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { Tag } from 'ng-tagcanvas';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: 'app.component.html',
+	styleUrls: [ 'app.component.css' ]
 })
 export class AppComponent {
-  title = 'app';
+	
+	title = 'app';
+
+	tags: Tag[] = [
+		{ weight: 16, text: 'insurance' },
+		{ weight: 24, text: 'lay' },
+		{ weight: 18, text: 'tense' },
+		{ weight: 16, text: 'cabin' },
+		{ weight: 32, text: 'bomb' },
+		{ weight: 30, text: 'broadcast' },
+		{ weight: 18, text: 'portion' },
+		{ weight: 20, text: 'progress' },
+		{ weight: 16, text: 'match' },
+		{ weight: 30, text: 'cover' }
+	];
+	
+	options: TagCanvasOptions = {
+		dragControl: true,
+		clickToFront: 500,
+		initial: [ .1, 0 ],
+		weight: true
+	};
+	
+	constructor() {
+		
+	}
+
+	tagClicked(tag) {
+		console.log(tag);
+	}
+	
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TagCanvasModule } from 'ng-tagcanvas';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		TagCanvasModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
